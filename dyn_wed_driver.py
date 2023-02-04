3d597010b3af05aaffb5444584b035af1408cb1a
SKY <sky012877@gmail.com>
2023-02-03 16:22:35 -0800

    dyn_wed_driver.py - Modified :

--- dyn_wed_driver.py
+++ dyn_wed_driver.py
@@ -3,16 +3,19 @@
 #!/bin/python
 import importlib
 from selenium import webdriver
+import multi_import as mu
+
 print("""Test import module for dynamic import other module
             You can check CONST in check module, save module, and push anykey for to see new value))
         """)
-
+#TODO CREATE timer!!!
 
 if __name__ == '__main__':
     driver = webdriver.Chrome()  # Optional argument, if not specified will search path.
     driver.get('https://www.google.com/')
-    menu_module = importlib.import_module("menu")
+    menu_module = mu.import_one("menu")
     while True:
-        menu = menu_module.Menu()
+        menu = menu_module.Menu(driver)
         menu.display()
-        importlib.reload(menu_module)
+        mu.reload(menu_module)
+        print("Next loop of menu")
